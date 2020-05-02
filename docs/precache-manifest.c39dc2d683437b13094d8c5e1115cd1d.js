803a08be5e26e49a42817c2b8f1753fc8caa9de3
SolAZDev <SolAZDev@gmail.com>
2020-05-02 17:34:37 -0400

    First Build

---
+++ docs/precache-manifest.c39dc2d683437b13094d8c5e1115cd1d.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a661291899ee46442005",
+    "url": "/css/app.ccd4e192.css"
+  },
+  {
+    "revision": "c81fb1924a94a9189448",
+    "url": "/css/chunk-vendors.0c5f5ead.css"
+  },
+  {
+    "revision": "eda057be8d7ff9e8d3a37581d36288dc",
+    "url": "/data/Dares.txt"
+  },
+  {
+    "revision": "9f11b4366824ff77bfb0b19dfd907b2e",
+    "url": "/data/DaresHot.txt"
+  },
+  {
+    "revision": "f2414ca7dc8c2aa3c506ea85da715ef8",
+    "url": "/data/DaresSexy.txt"
+  },
+  {
+    "revision": "ab43188f2ebb1b682d2cbff212a44504",
+    "url": "/data/DiceAction.txt"
+  },
+  {
+    "revision": "487bfeeab48b85be265b0b5926889351",
+    "url": "/data/DicePlace.txt"
+  },
+  {
+    "revision": "eaadd6055b0ad5cda3f757c26b790606",
+    "url": "/data/Truths.txt"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "/img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "bf42a2821f7100ec4065ab98c7e747f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bade127e0b483cdda955",
+    "url": "/js/about.9c212104.js"
+  },
+  {
+    "revision": "a661291899ee46442005",
+    "url": "/js/app.d23aa0a7.js"
+  },
+  {
+    "revision": "c81fb1924a94a9189448",
+    "url": "/js/chunk-vendors.db07ef55.js"
+  },
+  {
+    "revision": "3b25d3b58f3028738c5d0cd35c92801e",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
